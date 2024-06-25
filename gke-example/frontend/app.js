90d21e357a3e98cb12e84ff58f2b67300a0122a6
Gaby hdez <rosa.hernandezcu@alumno.buap.mx>
2024-06-25 17:40:16 +0000

    prss

--- gke-example/frontend/app.js
+++ gke-example/frontend/app.js
@@ -7,11 +7,12 @@ const port = 8081;
 app.get('/', async (req, res) => {
     try {
         //const response = await axios.get('http://backend-service:8080/data');
+        //prueba
         const response = await axios.get('http://127.0.0.1:8080/data');
         res.send(`
             <html>
                 <body>
-                    <h1>Datos de BigQuery</h1>
+                    <h1>Datos de BigQuery</h1><h1>Datos de BigQuery</h1>
                     <pre>${JSON.stringify(response.data, null, 2)}</pre>
                 </body>
             </html>
